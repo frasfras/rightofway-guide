8ac006450be17e0f482360507e0cd507e1733c35
frasfras <65541080+frasfras@users.noreply.github.com>
2022-03-29 03:55:16 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+   <div id="WeatherPlugin">
+   
+   </div>
+     <div class="input m-4">
+  
+  </div>
+  <div class="input m-4">
+  
+    
+    
+  </div><div class="input m-4">
+  
+  </div>
+   <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">Plan</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+<script>
+
+
+/*import { ToggleButton } from 'vue-js-toggle-button';*/
+
+export default {
+  name: 'App',
+   methods: { 
+          fetchWeather(){
+              fetch('https://api.openweathermap.org/data/2.5/weather?q=manila&appid=04086e1e40f5e8aad7854a1b88547698'
+              )
+              .then((res) => res.json())
+              .then((res) => {
+                    this.informations= res;
+                    console.log(res);
+              });
+  },
+  components: {
+   
+    
+  },
+   data() {
+    return {
+      gpu: false,
+      fairw: false,
+      cpu: false,
+      green: false
+    };
+  },
+  mounted(){
+    
+     this.fetchWeather();
+    },
+  
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
