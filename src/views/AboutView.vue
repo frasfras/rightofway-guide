a942421a41031fe81a729218d51c0408776d83ae
frasfras <65541080+frasfras@users.noreply.github.com>
2022-03-29 03:58:26 +0800

    Add files via upload

---
+++ src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    
+     <h1> </h1>
+  
+<iframe name ='PegaGadgetIfr' id ='PegaGadgetIfr' data-pega-iframe-gadgetname ='PegaGadget' data-pega-resizetype ='stretch' style= 'height: 400px; width: 100%;' src ='https://vqms5ksn.pegace.net/prweb/PRAuth/app/right-of-ways-management_7770/!Rowrequestoffer/?pzuiactionzzz=CXtpbn1qNWN2MHRBRFArcEt5dldFM1VFUythU3Fqb212UHd6MWZiUk5MSExhbG9sTkdPVFVvdUg2d1V3ckcyNnRKckJ6SGpuclJnNlJxQVV6NlhXKzR2SkN4MHNwUUtpTytleW1Ba2lqZjB5VFZlWjdjaDU3b0dEbUlUZmJqWFRFL1NTR0c5WWsrQUtIbmJJSkhyeHQ2OXVPZWltM3BKRWhRT0ZIZ2VuY213amF5cS9kZldhbTE5WHN2Y1JTaHlVMnN0N0FBbDEyRTNZVzhDT2VZTSs0THhYNkxpYWJZSUdVR0ZmcUd0cmhOaWowUFZ2V3JKRWUwcWkvK3YyNjhZVHZJdklOUzdKMzBLc3BJalNFd3FoelN3RDQ1Ynk2cGs4dGxtT3E5YkRYWVJaclRDZ3QwVGFqZExSMHVtWmx0eTFFY3lBUTJ1Um54RWVhdE8zZFkzNUZTNXF1bEs2YWQ5U0VydzF3cWZEazVQRnBkSU05YXBpVTVhK0tqa2tVdS9qVjY3L1A4b01OTHc1V1RyTWVmUzljdmpXZGxBPT0%3D*' ></iframe>
+  </div>
+  
+</template>
+
+
+
